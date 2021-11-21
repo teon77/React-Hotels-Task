929823a9bb4a439c2b8f9404802a4066e016325d
Eithan Refaely <eithanrefaely@gmail.com>
2021-11-21 12:21:11 +0200

    Basic Setup

---
+++ hotels-app/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+        
+
+        </div>
+      </Router>
+    )
+  }
+
+}
+
+export default App;
